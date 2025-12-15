b4b7c4caa5defbe72004220306b9e9520c534419
Qamarul00 <rulqam@gmail.com>
2025-12-15 13:23:09 +0800

    Update supabase.php

--- supabase.php
+++ supabase.php
@@ -2,7 +2,7 @@
 // supabase.php
 
 $SUPABASE_URL = "https://zhjzbvghigeuarxvucob.supabase.co";
-$SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+$SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoanpidmdoaWdldWFyeHZ1Y29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NzAxOTUsImV4cCI6MjA4MDM0NjE5NX0.TF0dz6huz6tPAiXe3pz04Fuafh7dewIVNqWpOzJbm2w";
 
 function supabaseRequest($endpoint) {
     global $SUPABASE_URL, $SUPABASE_KEY;
