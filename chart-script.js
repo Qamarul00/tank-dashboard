9e8c9b041fcdf36603c910dcb4097a409691a2e5
Qamarul00 <rulqam@gmail.com>
2025-12-18 02:01:14 +0800

    Add files via upload

--- chart-script.js
+++ chart-script.js
@@ -1,4 +1,4 @@
-// Supabase Configuration
+// Supabase Configuration chart-script.js
 const SUPABASE_URL = 'https://zhjzbvghigeuarxvucob.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoanpidmdoaWdldWFyeHZ1Y29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NzAxOTUsImV4cCI6MjA4MDM0NjE5NX0.TF0dz6huz6tPAiXe3pz04Fuafh7dewIVNqWpOzJbm2w';
 
@@ -1027,3 +1027,5 @@ window.testChartConnection = async function() {
         return false;
     }
 };
+
+//end
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Supabase Configuration
+// Supabase Configuration script.js
 const SUPABASE_URL = 'https://zhjzbvghigeuarxvucob.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoanpidmdoaWdldWFyeHZ1Y29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NzAxOTUsImV4cCI6MjA4MDM0NjE5NX0.TF0dz6huz6tPAiXe3pz04Fuafh7dewIVNqWpOzJbm2w';
 
@@ -1015,3 +1015,5 @@ window.testConnection = async function() {
         return false;
     }
 };
+
+//end
